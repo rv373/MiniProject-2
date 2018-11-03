b60804c3d2a4d3f073f01835134ed74298ffbf72
rv373 <rv373@njit.edu>
2018-11-03 03:02:23 -0400

    8.  Create a unit test to count the number of records inserted by the database seed.  i.e. $userCount = 50
     - Done.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(CarsTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,13 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
-        /*factory(App\User::class, 50)->create()->each(function ($u) {
+        factory(App\User::class, 50)->create()->each(function ($u) {
             //$u->posts()->save(factory(App\Post::class)->make());
-        });*/
-        DB::table('users')->insert([
+        });
+        /*DB::table('users')->insert([
             'name' => str_random(10),
             'name' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
-        ]);
+        ]);*/
     }
 }
