fd66c7f612b995502a344625c4e9db02620e90b4
rv373 <rv373@njit.edu>
2018-11-03 03:32:05 -0400

    11. Create a unit test to test deleting a car. - Done.

---
+++ tests/Unit/CarsDBSeederTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB as DB;
+use App\Car;
+
+class CarsDBSeederTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCarsDBSeeder()
+    {
+
+        $cars = DB::table('cars')->count();
+        echo $cars;
+        $this->assertEquals(50, $cars, "Cars DB Seeder Count Test Success");
+
+    }
+}
+
